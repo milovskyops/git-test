c6142922007522f9925998fea002228963a2eaaa
Milovsky <konstantin.milovskiy@gmail.com>
2022-12-03 12:46:26 +0300

    Change money

---
+++ invok.py
@@ -0,0 +1,13 @@
+money = 12
+price_cola = 100
+price_water = 30 
+price_milk = 60 
+
+if money >= price_cola:
+    print('we can buy cola')
+elif money >= price_milk and money < price_cola:
+    print('we can buy milk')
+elif  money >= price_water and money <= price_milk:
+    print('we can buy milk')
+else:
+    print('we dont even have enough money for water')    
